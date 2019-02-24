7b56997bd205155826ca061417ca921fd0140d34
Hepson Sanchez <hepsonms6@gmail.com>
2019-02-24 06:00:37 -0600

    Made Box check pin entered

--- Box.php
+++ Box.php
@@ -1,4 +1,6 @@
-
+<?php
+include "base.php";
+?>
 <!DOCTYPE HTML>
 <!--
 	Spectral by HTML5 UP
@@ -21,67 +23,52 @@
 				<!-- Header -->
 					<header id="header">
 						<h1><a href="index.html">Foodbox</a></h1>
-						<nav id="nav">
-							<!--<ul>
-								<li class="special">
-									<a href="#menu" class="menuToggle"><span>Menu</span></a>
-									<div id="menu">
-										<ul>
-											<li><a href="index.html">Home</a></li>
-											<li><a href="generic.html">Generic</a></li>
-											<li><a href="elements.html">Elements</a></li>
-											<li><a href="#">Sign Up</a></li>
-											<li><a href="#">Log In</a></li>
-										</ul>-->
-									</div>
-								</li>
-							</ul>
-						</nav>
 					</header>
 
 				<!-- Main -->
 					<article id="main">
 						<header>
-							<h2>Enter your code</h2>
-                            <form action="form1.php" method="post">
-
-                            <input type="text" name="number" value = "0" /><br />
-                            <br />
-                            <input type="submit" name="checkform" value="Submit" />
-                            </form> 
-                            <br />
-                            <?
-                            if  (isset($_POST['checkform'])) 
-                            {
-                            if  (( $_POST["number"] ) <=10 )  
-                                echo "The number you entered is less than or equal to  10";
-                                else
-                                echo "The number you entered is Greater than 10";
-
-                            }
-                            ?>
-                            
+						<?php
+							if($_SESSION['Unlocked'] == "True")
+							{
+								echo "<h3>Box has been unlocked</h3>";
+								$_SESSION['Unlocked'] = "False";
+                                echo "<a href='Box.php'>Close Box</a>";
+							}
+							elseif(!empty($_POST['Pin']))
+							{
+								$pin = mysqli_real_escape_string($dbcon, $_POST['Pin']);
+								$box_id = 3333;
+								//$box_id = $_SESSION['Box_id'];
+								
+								$check_volunteer = mysqli_query($dbcon, "SELECT * FROM Delivery 
+																		 WHERE Pin = '".$pin."' AND Box_ID = '".$box_id."'");
+								
+								if(mysqli_num_rows($check_volunteer) == 1)
+								{
+									$_SESSION['Unlocked'] = "True";
+									echo "<meta http-equiv='refresh' content='0;Box.php'/>";
+								}
+								else
+								{
+									echo "<h1>Wrong Pin Entered</h1>";
+									echo "<meta http-equiv='refresh' content='2;Box.php'/>";
+								}
+							}
+							else
+							{?>
+								<h2>Enter your code</h2>
+								<form action="Box.php" method="post">
+									<input type="number" name="Pin" min="1111" max="9999" /><br />
+									<br />
+									<input type="submit" name="checkform" value="Submit" />
+								</form>
+							<?php
+							}
+						?>
+						<br/>
 						</header>
-                        <!--
-						<section class="wrapper style5">
-							<div class="inner">
-
-								<h3>Lorem ipsum dolor</h3>
-								<p>Morbi mattis mi consectetur tortor elementum, varius pellentesque velit convallis. Aenean tincidunt lectus auctor mauris maximus, ac scelerisque ipsum tempor. Duis vulputate ex et ex tincidunt, quis lacinia velit aliquet. Duis non efficitur nisi, id malesuada justo. Maecenas sagittis felis ac sagittis semper. Curabitur purus leo, tempus sed finibus eget, fringilla quis risus. Maecenas et lorem quis sem varius sagittis et a est. Maecenas iaculis iaculis sem. Donec vel dolor at arcu tincidunt bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce ut aliquet justo. Donec id neque ipsum. Integer eget ultricies odio. Nam vel ex a orci fringilla tincidunt. Aliquam eleifend ligula non velit accumsan cursus. Etiam ut gravida sapien.</p>
-
-								<p>Vestibulum ultrices risus velit, sit amet blandit massa auctor sit amet. Sed eu lectus sem. Phasellus in odio at ipsum porttitor mollis id vel diam. Praesent sit amet posuere risus, eu faucibus lectus. Vivamus ex ligula, tempus pulvinar ipsum in, auctor porta quam. Proin nec dui cursus, posuere dui eget interdum. Fusce lectus magna, sagittis at facilisis vitae, pellentesque at etiam. Quisque posuere leo quis sem commodo, vel scelerisque nisi scelerisque. Suspendisse id quam vel tortor tincidunt suscipit. Nullam auctor orci eu dolor consectetur, interdum ullamcorper ante tincidunt. Mauris felis nec felis elementum varius.</p>
-
-								<hr />
-
-								<h4>Feugiat aliquam</h4>
-								<p>Nam sapien ante, varius in pulvinar vitae, rhoncus id massa. Donec varius ex in mauris ornare, eget euismod urna egestas. Etiam lacinia tempor ipsum, sodales porttitor justo. Aliquam dolor quam, semper in tortor eu, volutpat efficitur quam. Fusce nec fermentum nisl. Aenean erat diam, tempus aliquet erat.</p>
-
-								<p>Etiam iaculis nulla ipsum, et pharetra libero rhoncus ut. Phasellus rutrum cursus velit, eget condimentum nunc blandit vel. In at pulvinar lectus. Morbi diam ante, vulputate et imperdiet eget, fermentum non dolor. Ut eleifend sagittis tincidunt. Sed viverra commodo mi, ac rhoncus justo. Duis neque ligula, elementum ut enim vel, posuere finibus justo. Vivamus facilisis maximus nibh quis pulvinar. Quisque hendrerit in ipsum id tellus facilisis fermentum. Proin mauris dui, at vestibulum sit amet, auctor bibendum neque.</p>
-
-							</div>
-						</section>
 					</article>
--->
 				<!-- Footer -->
 					<footer id="footer">
 						<ul class="icons">
