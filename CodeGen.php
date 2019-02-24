5f759c5f3f805f8ca7bfa9cfab8ac4375c047bbb
Hepson Sanchez <hepsonms6@gmail.com>
2019-02-23 19:50:07 -0600

    changed length to 4, only digits

--- CodeGen.php
+++ CodeGen.php
@@ -5,8 +5,9 @@
  * @copyright 2019
  */
 
-function generateRandomString($length = 10) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+function generateRandomString($length = 4) {
+    //$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $characters = '0123456789';
     $charactersLength = strlen($characters);
     $randomString = '';
     for ($i = 0; $i < $length; $i++) {
